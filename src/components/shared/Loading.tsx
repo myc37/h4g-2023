7264d8e0251470b15c010a583862c2c758a92732
raynerljm <raynerljm@gmail.com>
2023-02-10 14:57:19 +0800

    feat: update loading screen

--- src/components/shared/Loading.tsx
+++ src/components/shared/Loading.tsx
@@ -1,5 +1,23 @@
+import { Box, Spinner, Text } from '@chakra-ui/react';
+
 const Loading = () => {
-  return <p className="p-4 w-full h-full text-center">Loading...</p>;
+  return (
+    <Box
+      h="fill-available"
+      bg="primary.50"
+      minH="100vh"
+      display="flex"
+      flexDir="column"
+      justifyContent="center"
+      alignItems="center"
+      gap="12px"
+    >
+      <Text textStyle="h5" color="primary.700">
+        Loading...
+      </Text>
+      <Spinner color="primary.700" />
+    </Box>
+  );
 };
 
 export default Loading;
