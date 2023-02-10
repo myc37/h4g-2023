df8ba747358dbaa73653830700f69b2670e260a9
raynerljm <raynerljm@gmail.com>
2023-02-10 18:26:20 +0800

    fix: remove head in index

--- src/components/screens/Index.tsx
+++ src/components/screens/Index.tsx
@@ -1,11 +1,9 @@
 import { Box } from '@chakra-ui/react';
-import { Head } from '~/components/shared/Head';
 
 function Index() {
   return (
     <Box bg="primary.50" px="24px" pb="40px">
       <Box w="full" maxW="1140px">
-        <Head title="TOP PAGE" />
         <div className="hero min-h-screen">
           <div className="text-center hero-content" />
         </div>
